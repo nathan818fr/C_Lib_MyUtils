bdf49edc66004f948c9be5ad58d8245df0701e45
Nathan Poirier <nathan@poirier.io>
2015-02-18 11:51:01 +0100

    add my_bool

---
+++ include/myutils_bool.h
@@ -0,0 +1,18 @@
+/*
+** myutils_bool.h for myutils in /repo/Epitech/C_Lib_MyUtils/include
+** 
+** Made by Nathan Poirier
+** Login   <poirie_n@epitech.net>
+** 
+** Started on  Wed Feb 18 11:49:28 2015 Nathan Poirier
+** Last update Wed Feb 18 11:50:44 2015 Nathan Poirier
+*/
+
+#ifndef MYUTILS_BOOL_H_
+# define MYUTILS_BOOL_H_
+
+# define MY_TRUE	1;
+# define MY_FALSE	0;
+typedef char		t_my_bool;
+
+#endif /* !MYUTILS_BOOL_H_ */
