2bf28476cac85aa39caf82cd88d633b75dbc1d43
Nathan Poirier <nathan@poirier.io>
2015-05-21 12:57:30 +0200

    add my_databuf_insert

--- include/myutils_databuf.h
+++ include/myutils_databuf.h
@@ -5,7 +5,7 @@
 ** Login   <poirie_n@epitech.net>
 ** 
 ** Started on  Tue May 19 18:56:20 2015 Nathan Poirier
-** Last update Tue May 19 22:04:29 2015 Nathan Poirier
+** Last update Thu May 21 12:57:15 2015 Nathan Poirier
 */
 
 #ifndef MYUTILS_DATABUF_H_
@@ -30,6 +30,7 @@ void		my_databuf_free(t_databuf *db);
 ** my_databuf_append.c
 */
 int		my_databuf_append(t_databuf *db, char *buf, int len);
+int		my_databuf_insert(t_databuf *db, char *buf, int len, int at);
 
 /*
 ** my_databuf_alloc.c
--- src/my_databuf/my_databuf_append.c
+++ src/my_databuf/my_databuf_append.c
@@ -5,7 +5,7 @@
 ** Login   <poirie_n@epitech.net>
 ** 
 ** Started on  Tue May 19 19:01:14 2015 Nathan Poirier
-** Last update Tue May 19 22:09:07 2015 Nathan Poirier
+** Last update Thu May 21 12:56:08 2015 Nathan Poirier
 */
 
 #include "myutils.h"
@@ -29,6 +29,31 @@ int		my_databuf_append(t_databuf *db, char *buf, int len)
       db->len++;
       i++;
     }
+  if (db->append_zero)
+    db->buf[db->len] = 0;
+  return (0);
+}
+
+int		my_databuf_insert(t_databuf *db, char *buf, int len, int at)
+{
+  int		i;
+
+  if (buf == NULL || len <= 0 || at > db->len)
+    return (0);
+  if (db->len + len >= db->size)
+    {
+      if (my_databuf_alloc(db, len) == -1)
+	return (-1);
+    }
+  i = 0;
+  while (i < len)
+    {
+      if (at + i < db->len)
+	db->buf[at + len + i] = db->buf[at + i];
+      db->buf[at + i] = buf[i];
+      i++;
+    }
+  db->len += len;
   if (db->append_zero)
     db->buf[db->len] = 0;
   return (0);
